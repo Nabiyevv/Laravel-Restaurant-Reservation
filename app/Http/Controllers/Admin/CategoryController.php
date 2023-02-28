de55259a8b74a0498c18f0ffb7532be66172e918
Mahamad Nabiyev <maqa.nabiev@gmail.com>
2023-02-28 22:45:19 +0400

    Delete and Edit Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
@@ -30,9 +32,19 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $path = $request->file('image')->store('public/categories');
+        // Generate a file name with extension
+        //$fileName = 'profile-'.time().'.'.$file->getClientOriginalExtension();
+
+        Category::create([
+            'image'=> $path,
+            'description' => $request->description,
+            'name' => $request->name,
+        ]);
+        $categories = Category::all();
+        return view('admin.categories.index',compact('categories'));
     }
 
     /**
@@ -46,24 +58,41 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit',compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,Category $category)
     {
-        //
+        $request->validate([
+            'name'=>['required'],
+            'description'=>['required'],
+        ]);
+        $path = $category->image;
+        if($request->hasFile('image')){
+            Storage::delete($path);
+            $path = $request->file('image')->store('public/categories');
+        }
+        Category::where('id',$category->id)->update([
+            'image' => $path,
+            'name' =>$request->name,
+            'description' =>$request->description,
+        ]);
+
+        return to_route('admin.categories.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        Storage::delete($category->image);
+        $category->delete();
+        return to_route('admin.categories.index');
     }
 }
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -23,13 +23,14 @@
                     </div>
                 @endif
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10 ">
-                    <form method="POST" enctype="multipart/form-data" action="{{ route('admin.categories.store') }}">
+                    <form method="POST" enctype="multipart/form-data" action="{{ route('admin.categories.update',$category->id) }}">
                         @csrf
+                        @method("PUT")
                         <div class="sm:col-span-6">
                             <label for="name" class="block text-sm font-medium text-gray-700 dark:text-white"> Name
                             </label>
                             <div class="mt-1">
-                                <input type="text" id="name" name="name"
+                                <input type="text" id="name" name="name" value="{{ $category->name }}"
                                     class="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
                             </div>
                         </div>
@@ -37,6 +38,7 @@
                             <label for="image" class="block text-sm font-medium text-gray-700 dark:text-white"> Image
                             </label>
                             <div class="mt-1">
+                                <img src="{{ Storage::url($category->image) }}" alt="" class=" h-16 rounded">
                                 <input type="file" id="image" name="image"
                                     class="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
                             </div>
@@ -47,7 +49,11 @@
                                 class="block text-sm font-medium text-gray-700 dark:text-white">Description</label>
                             <div class="mt-1">
                                 <textarea id="description" rows="3" name="description"
-                                    class="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror"></textarea>
+                                    class="dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+                                     shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base
+                                      leading-normal transition duration-150 ease-in-out focus:border-indigo-500 
+                                      block w-full sm:text-sm border-gray-300 rounded-md
+                                       @error('name') border-red-400 @enderror">{{ $category->description }}</textarea>
                             </div>
 
                         </div>
